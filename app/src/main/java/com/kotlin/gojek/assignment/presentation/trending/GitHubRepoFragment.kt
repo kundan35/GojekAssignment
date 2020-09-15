576dc3e7e0ff9d2341514ecb783c1be00f8dfca5
Kundan Kumar <kundan@logistimo.com>
2020-09-15 12:37:05 +0530

    added error state layout

--- app/src/main/java/com/kotlin/gojek/assignment/presentation/trending/GitHubRepoFragment.kt
+++ app/src/main/java/com/kotlin/gojek/assignment/presentation/trending/GitHubRepoFragment.kt
@@ -51,6 +51,7 @@ class GitHubRepoFragment : DaggerFragment(), OnGitHubRepoAdapterListener {
                 if (visibility) {
                     fragmentGithubRepoBinding.githubRepoProgressBar.visibility =
                         View.GONE
+                    fragmentGithubRepoBinding.noInternetLayout.noInternetConstraintLayout.visibility = View.GONE
                 } else {
                     fragmentGithubRepoBinding.githubRepoProgressBar.visibility = View.VISIBLE
                 }
@@ -65,6 +66,7 @@ class GitHubRepoFragment : DaggerFragment(), OnGitHubRepoAdapterListener {
 
         viewModel.errMsgLiveData.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
             Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+            fragmentGithubRepoBinding.noInternetLayout.noInternetConstraintLayout.visibility = View.VISIBLE
         })
 
     }
